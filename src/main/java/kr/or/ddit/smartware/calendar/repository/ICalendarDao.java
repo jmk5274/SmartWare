d5eeea1bb983c5b8d4cc47b99e9ab4fe31cfbbe2
jms <mingsoo0807@gmail.com>
2019-10-08 09:21:28 +0900

    FullCalendar 출력 / DB 일정 출력(e003)
    - fullcalendar는 조금 더 다듬을 예정
    - db관련 기능 추가 예정
    - controller, dao, service 구현
    
    * 테스트용 계정 e001 --> e003

--- src/main/java/kr/or/ddit/smartware/calendar/repository/ICalendarDao.java
+++ src/main/java/kr/or/ddit/smartware/calendar/repository/ICalendarDao.java
@@ -1,5 +1,14 @@
 package kr.or.ddit.smartware.calendar.repository;
 
+import java.util.List;
+
+import kr.or.ddit.smartware.calendar.model.DepCalendar;
+import kr.or.ddit.smartware.calendar.model.EmpCalendar;
+
 public interface ICalendarDao {
+
+	List<EmpCalendar> getEmpCalendarList(String emp_id);
+
+	List<DepCalendar> getDepCalendarList(String emp_id);
 
 }
