501d4fe65477f0254bfba3f71ba3bcad437d50af
jms <mingsoo0807@gmail.com>
2019-10-21 12:12:40 +0900

    세션 logger 제거

--- src/main/java/kr/or/ddit/smartware/interceptor/SessionCheckInterceptor.java
+++ src/main/java/kr/or/ddit/smartware/interceptor/SessionCheckInterceptor.java
@@ -4,20 +4,18 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import kr.or.ddit.smartware.employee.model.Employee;
 
 public class SessionCheckInterceptor extends HandlerInterceptorAdapter {
-	private static final Logger logger = LoggerFactory.getLogger(SessionCheckInterceptor.class);
+	
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
 		HttpSession session = request.getSession();
 		Employee employee = (Employee) session.getAttribute("S_EMPLOYEE");
-		logger.debug("이곳은 sessioncheck");
+		
 		// session에 user가 없으면 login화면으로 이동
 		if(employee == null) {
 			response.sendRedirect(request.getContextPath() + "/login");
