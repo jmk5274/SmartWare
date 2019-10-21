b317aca6db6b5115da100ee02d81629cddf64f25
jms <mingsoo0807@gmail.com>
2019-10-21 10:14:52 +0900

    sessioncheckinterceptor 살림

--- src/main/java/kr/or/ddit/smartware/interceptor/SessionCheckInterceptor.java
+++ src/main/java/kr/or/ddit/smartware/interceptor/SessionCheckInterceptor.java
@@ -6,19 +6,21 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
+import kr.or.ddit.smartware.employee.model.Employee;
+
 public class SessionCheckInterceptor extends HandlerInterceptorAdapter {
 
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
 		HttpSession session = request.getSession();
-//		User user = (User) session.getAttribute("S_USERVO");
+		Employee employee = (Employee) session.getAttribute("S_EMPLOYEE");
 		
 		// session에 user가 없으면 login화면으로 이동
-//		if(user == null) {
-//			response.sendRedirect(request.getContextPath() + "/login");
-//			return false;
-//		}
+		if(employee == null) {
+			response.sendRedirect(request.getContextPath() + "/login");
+			return false;
+		}
 		
 		// session에 user가 있으면 존재하면 통과
 		return true;
