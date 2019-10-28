80203f65c0f9c7971c6f4703957cff0236d4781e
jmk <jmk5274@gmail.com>
2019-10-28 16:10:59 +0900

    메신저 download파일 한글처리

--- src/main/java/kr/or/ddit/smartware/messenger/web/MessengerController.java
+++ src/main/java/kr/or/ddit/smartware/messenger/web/MessengerController.java
@@ -3,6 +3,7 @@ package kr.or.ddit.smartware.messenger.web;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -420,14 +421,28 @@ public class MessengerController {
 	}
 	
 	@RequestMapping("chatFile")
-	public void chatFile(String path, HttpServletResponse response, HttpSession session) throws IOException {
+	public void chatFile(String path, HttpServletRequest request, HttpServletResponse response, HttpSession session) throws IOException {
 		ServletOutputStream sos = null;
 		FileInputStream fis = null;
 		File file = null;
-		
+		String header = getBrowser(request);
+
 		String fileName = path.substring(path.lastIndexOf("/")+1);
 		
-		response.setHeader("Content-Disposition", "attachment;filename="+fileName);
+		if (header.contains("MSIE")) {
+		  String docName = URLEncoder.encode(fileName,"UTF-8").replaceAll("\\+", "%20");
+		  response.setHeader("Content-Disposition", "attachment;filename=" + docName + ";");
+		} else if (header.contains("Firefox")) {
+		  String docName = new String(fileName.getBytes("UTF-8"), "ISO-8859-1");
+		  response.setHeader("Content-Disposition", "attachment; filename=\"" + docName + "\"");
+		} else if (header.contains("Opera")) {
+		  String docName = new String(fileName.getBytes("UTF-8"), "ISO-8859-1");
+		  response.setHeader("Content-Disposition", "attachment; filename=\"" + docName + "\"");
+		} else if (header.contains("Chrome")) {
+		  String docName = new String(fileName.getBytes("UTF-8"), "ISO-8859-1");
+		  response.setHeader("Content-Disposition", "attachment; filename=\"" + docName + "\"");
+		}
+		
 		response.setHeader("Content-Transfer-Encoding", "binary");
 		try {
 			sos = response.getOutputStream();
@@ -453,14 +468,29 @@ public class MessengerController {
 	}
 	
 	@RequestMapping("chatImgFile")
-	public void chatImgFile(String path, HttpServletResponse response, HttpSession session) throws IOException {
+	public void chatImgFile(String path, HttpServletRequest request, HttpServletResponse response, HttpSession session) throws IOException {
 		ServletOutputStream sos = null;
 		FileInputStream fis = null;
 		File file = null;
 		
+		String header = getBrowser(request);
+		
 		String fileName = path.substring(path.lastIndexOf("/")+1);
 		
-		response.setHeader("Content-Disposition", "attachment;filename="+fileName);
+		if (header.contains("MSIE")) {
+			  String docName = URLEncoder.encode(fileName,"UTF-8").replaceAll("\\+", "%20");
+			  response.setHeader("Content-Disposition", "attachment;filename=" + docName + ";");
+			} else if (header.contains("Firefox")) {
+			  String docName = new String(fileName.getBytes("UTF-8"), "ISO-8859-1");
+			  response.setHeader("Content-Disposition", "attachment; filename=\"" + docName + "\"");
+			} else if (header.contains("Opera")) {
+			  String docName = new String(fileName.getBytes("UTF-8"), "ISO-8859-1");
+			  response.setHeader("Content-Disposition", "attachment; filename=\"" + docName + "\"");
+			} else if (header.contains("Chrome")) {
+			  String docName = new String(fileName.getBytes("UTF-8"), "ISO-8859-1");
+			  response.setHeader("Content-Disposition", "attachment; filename=\"" + docName + "\"");
+			}
+		
 		response.setHeader("Content-Transfer-Encoding", "binary");
 		try {
 			sos = response.getOutputStream();
@@ -484,4 +514,20 @@ public class MessengerController {
 			}
 		}
 	}
+	
+	private String getBrowser(HttpServletRequest request) {
+		  String header = request.getHeader("User-Agent");
+
+		  if (header.contains("MSIE")) {
+		    return "MSIE";
+		  } else if(header.contains("Trident")) {
+		    return "MSIE11";
+		  } else if(header.contains("Chrome")) {
+		    return "Chrome";
+		  } else if(header.contains("Opera")) {
+		    return "Opera";
+		}
+
+		  return "Firefox";
+		}
 }
